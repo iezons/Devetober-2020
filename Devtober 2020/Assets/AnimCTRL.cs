766fe78585e6d1300287e2d3ade3d4a6e731689a
Lawrence <2793500992@qq.com>
2020-10-13 19:27:33 -0400

    Model Import

---
+++ Devtober 2020/Assets/AnimCTRL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimCTRL : MonoBehaviour
+{
+    public Animator Door;
+    public Animator NPC;
+    public Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            Debug.Log("Get In Locker");
+            Door.SetTrigger("Open");
+            //float playtime = NPC.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            NPC.Play("GetInLock", 0, 0f);
+        }
+    }
+}
