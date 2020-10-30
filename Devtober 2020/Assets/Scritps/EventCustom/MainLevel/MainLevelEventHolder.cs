66709432419bcb57f47875818ef05fcda2c5c192
Lawrence <2793500992@qq.com>
2020-10-29 20:16:01 -0400

    Ready for pull

---
+++ Devtober 2020/Assets/Scritps/EventCustom/MainLevel/MainLevelEventHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MainLevelEventHolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        
+    }
+
+    void a(string name, UnityAction action)
+    {
+        EventCenter.GetInstance().AddEventListener(name, action);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
